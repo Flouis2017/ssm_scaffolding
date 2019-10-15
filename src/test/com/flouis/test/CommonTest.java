cfd82ad9745e404085348251f26b2cad4d88a9ca
Flouis2017 <315587951@qq.com>
2019-10-15 21:51:53 +0800

    HttpUtil test

--- src/test/com/flouis/test/CommonTest.java
+++ src/test/com/flouis/test/CommonTest.java
@@ -1,7 +1,13 @@
 package com.flouis.test;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.flouis.util.HttpUtil;
+import com.google.common.collect.Maps;
 import org.apache.commons.codec.digest.DigestUtils;
 import org.junit.Test;
+
+import java.util.Map;
 
 public class CommonTest {
 
@@ -22,6 +28,18 @@ public class CommonTest {
 
 		str = "430430";
 		System.out.println(DigestUtils.md5Hex(str)); // 291ad281e640429285d30ff25b66813d
+	}
+
+	@Test
+	public void httpUtilTest(){
+		String reqUrl = "https://test.a1life.cn/user/getUserByMobile";
+		Map argMap = Maps.newHashMap();
+		argMap.put("phoneNum", "18760537263");
+		JSONObject jsonRes = JSON.parseObject(HttpUtil.get(reqUrl, argMap));
+		System.out.println("GET Result:\n" + jsonRes);
+
+		jsonRes = JSON.parseObject(HttpUtil.post(reqUrl, argMap));
+		System.out.println("POST Result:\n" + jsonRes);
 	}
 
 }
